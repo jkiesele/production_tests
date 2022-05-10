fce00ea83c063a484348ed3f96d0ffa6f5e28ce4
Jan Kieseler <jkiesele@cern.ch>
2022-05-10 19:22:23 +0200

    made a bit simpler

--- run_nano.sh
+++ run_nano.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+## USAGE
+# events input_no outdir_id nparticles nthreads
+
+cmssw_install="/afs/cern.ch/user/j/jkiesele/work/HGCal/pepr_production/May2022/CMSSW_12_1_1/src"
+output_basedir="/eos/home-${USER:0:1}/${USER}/ML4Reco/$3"
+
+#no adaptations below needed
+echo "output will be copied to ${output_basedir}"
+
 maxEvents=$1
 declare -i inputnumber=$2
 
 finalfile="${inputnumber}_nanoML.root"
-outdir="/eos/cms/store/user/kelong/ML4Reco/$3"
-eosoutdir="root://eosuser.cern.ch/$outdir"
-if [ ! -d $outdir ]; then
-    mkdir $outdir
-fi
+outdir="${output_basedir}/$3"
 
 nParticles=10
 if [ $# -gt 3 ]; then
@@ -23,7 +28,7 @@ if [ $# -gt 4 ]; then
     numThreads=$5
 fi
 
-pushd /afs/cern.ch/user/k/kelong/work/ML4Reco/CMSSW_11_3_0_pre3/src
+pushd $cmssw_install
 eval `scramv1 runtime -sh`
 popd
 
@@ -38,4 +43,4 @@ fi
 cmsRun RECO.py inputFiles="file:$gsd" outputFile=$recoout nThreads=$numThreads
 cmsRun nanoML_cfg.py inputFiles=file:$recoout outputFile=file:$finalfile nThreads=$numThreads
 echo "${inputnumber} done"
-xrdcp $finalfile $eosoutdir/$finalfile 
+mv $finalfile $outdir/$finalfile 
